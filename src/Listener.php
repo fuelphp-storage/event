948cb24877905a0f65069b0bb3e303ac077a66c7
WanWizard <wanwizard@wanwizard.eu>
2014-05-29 17:40:40 +0200

    updated copyright info

--- src/Listener.php
+++ src/Listener.php
@@ -4,7 +4,7 @@
  * @version    2.0
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010 - 2013 Fuel Development Team
+ * @copyright  2010 - 2014 Fuel Development Team
  * @link       http://fuelphp.com
  */
 
